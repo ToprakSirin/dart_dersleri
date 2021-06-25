bf6e01a9753515a43a679328d449dfe664956a7a
ToprakSirin <sirintprk21@gmail.com>
2021-06-25 09:58:33 +0300

    Polimorfizm Çok biçimlilik işlemi yapıldı.

--- bolum6/kalitima_giris.dart
+++ bolum6/kalitima_giris.dart
@@ -1,12 +1,29 @@
 main(List<String> args) {
   User user1 = User();
   user1.girisYap();
-  NormalUser normal1 = NormalUser();
-  normal1.davetEt();
-  SadeceOkuyabilenNormalUser user = SadeceOkuyabilenNormalUser();
-  user.girisYap();
+  var user2 = NormalUser();
+  user2.davetEt();
+  SadeceOkuyabilenNormalUser user3 = SadeceOkuyabilenNormalUser();
+  user3.girisYap();
 
-  
+  AdminUser user4 = AdminUser();
+
+  User user5 = AdminUser();
+  User user6 = SadeceOkuyabilenNormalUser(); //upcasting yukarı çevrim
+
+  List<User> tumUserler = [];
+  tumUserler.add(user1);
+  tumUserler.add(user2);
+  tumUserler.add(user3);
+
+  test(user1);
+  test(user2);
+  test(user3);
+  test(user4);
+}
+
+void test(User kullanici) {
+  kullanici.girisYap();
 }
 
 class User {
@@ -36,12 +53,16 @@ class SadeceOkuyabilenNormalUser extends NormalUser {
 
   @override
   void girisYap() {
-    print("Sadece Okuyabilen Norma lUser giriş yaptı");
+    print("Sadece Okuyabilen Normal User giriş yaptı");
   }
 }
 
 class AdminUser extends User {
   void toplamKullaniciSayisiniGoster() {
     print("Toplam user sayısı 20");
+  }
+  @override
+  void girisYap() {
+    print("Admin User giriş yaptı");
   }
 }
