fc451cb58ad5fa128ac7060c0f79216f24bbb349
ToprakSirin <sirintprk21@gmail.com>
2021-06-25 14:27:41 +0300

    final ve const Kavramları

---
+++ bolum7/final_const.dart
@@ -0,0 +1,38 @@
+main(List<String> args) {
+/*   var str = "emre";
+  str = "altunbilek";
+
+  final String str2 = "emre";
+  str2 = "altunbilek";
+
+  const String str3 = "emre";
+  str3 = "altunbilek"; */
+
+  /* const sayi = 5;
+  final sayi2 = 10;
+
+  final tarih = DateTime.now();
+  const tarih2=DateTime.now(); */
+
+/*   final liste = [1, 2, 3];
+  final liste2 = [1, 2, 3];
+
+  liste.add(4);
+  liste2.add(8);
+
+  if (liste == liste2) {
+    print("Eşit");
+  } else {
+    print("Eşit değil");
+  } */
+
+  //canonicalized
+  const liste = [1, 2];
+  const liste2 = [1, 2];
+
+    if (liste == liste2) {
+    print("Eşit");
+  } else {
+    print("Eşit değil");
+  } 
+}
