73576fe80c4f8b6ac79c03ae0c93ba98f4bd5052
ToprakSirin <sirintprk21@gmail.com>
2021-06-26 12:04:52 +0300

    Generic Tipleri Kullanarak Sınıf Olusturma

---
+++ bolum9/myStack.dart
@@ -0,0 +1,46 @@
+class MyStack {
+  List _listem = [];
+
+  push(yenEleman) {
+    _listem.add(yenEleman);
+  }
+
+  pop() {
+    return _listem.removeLast();
+  }
+}
+
+class IntMyStack {
+  List<int> _listem = <int>[];
+
+  void push(int yenEleman) {
+    _listem.add(yenEleman);
+  }
+
+  int pop() {
+    return _listem.removeLast();
+  }
+}
+
+class StringMyStack {
+  List<String> _listem = <String>[];
+
+  void push(String yenEleman) {
+    _listem.add(yenEleman);
+  }
+
+  String pop() {
+    return _listem.removeLast();
+  }
+}
+
+class GenericStack<T> {
+  List<T> _listem = <T>[];
+  void push(T yenEleman) {
+    _listem.add(yenEleman);
+  }
+
+  T pop() {
+    return _listem.removeLast();
+  }
+}
