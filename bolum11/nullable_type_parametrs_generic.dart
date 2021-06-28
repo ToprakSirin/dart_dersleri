7d7f54af93427cf75a3930705d6c6bd779e32aa7
ToprakSirin <sirintprk21@gmail.com>
2021-06-28 10:22:32 +0300

    nullable type parametrsgeneric

--- bolum11/nullable_type_parametrs_generic.dart
+++ bolum11/nullable_type_parametrs_generic.dart
@@ -1,11 +1,11 @@
 main(List<String> args) {
   List<String> stringListesi = ['emre', 'hasan', 'ali'];
   List<String>? nullOlabilecekStringListesi;
-  List<String
-  ?> nullOlabilecekStringLeriTutanListe = ['emre', null, 'hasan'];
+  List<String?> nullOlabilecekStringLeriTutanListe = ['emre', null, 'hasan'];
 
   print('string listesi $stringListesi');
   print('null olabilecek liste $nullOlabilecekStringListesi ');
   print(
       'nullOlabilecekStringLeriTutanListe $nullOlabilecekStringLeriTutanListe');
+  
 }
