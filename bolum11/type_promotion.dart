2d9c2d2594ef41e0ef1198aa9923f8b0c9715289
ToprakSirin <sirintprk21@gmail.com>
2021-06-28 10:18:11 +0300

    Type Promotion

---
+++ bolum11/type_promotion.dart
@@ -0,0 +1,15 @@
+main(List<String> args) {
+  String? mesaj;
+
+  if (DateTime.now().hour < 12) {
+    mesaj = "gunaydın";
+  } else {
+    mesaj = "iyi aksamlar";
+  }
+  print(mesaj);
+  print(mesaj.length);
+
+//type promotion from base class to derived class
+  Object metin = 'bu bir string';
+  if (metin is String) print(metin.length);
+}
