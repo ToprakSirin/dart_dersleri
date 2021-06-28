240a36318613c211cf1640019be56a9025818848
ToprakSirin <sirintprk21@gmail.com>
2021-06-28 10:23:51 +0300

    null assertion operator

---
+++ bolum11/null_assertion_operator.dart
@@ -0,0 +1,12 @@
+main(List<String> args) {
+  int nullOlabilirAmaDegil = 1;
+  List<int?> nullDegerTutanListe = [2, 3, null];
+
+  int a = nullOlabilirAmaDegil;
+  int b = nullDegerTutanListe.first!;
+  //int c = nullDondurebilirAmaDondurmeyecek()!.abs();
+
+/*   int? nullDondurebilirAmaDondurmeyecek() {
+    return 5;
+  } */
+}
